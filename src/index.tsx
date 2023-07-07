f922d528b252c1bc2281d7a53328f7b9ab5f7e79
Morji <morjimed@gmail.com>
2023-07-07 23:55:47 +0200

    all ui pages except admin list challenge

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from 'modules/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Student from 'modules/Authentication/Student/Student';
+import 'sass';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Student />
   </React.StrictMode>
 );
 
